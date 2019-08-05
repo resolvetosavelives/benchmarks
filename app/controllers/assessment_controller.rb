cea307c845cafd9eb687b0673b5c30ab0c99c488
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-05 18:08:46 -0400

    Rework the assessment framework and iterate over the whole thing

--- app/controllers/assessment_controller.rb
+++ app/controllers/assessment_controller.rb
@@ -2,12 +2,8 @@ class AssessmentController < ApplicationController
   def show
     #country = params[:country]
 
-    version = 'jee_v1'
     file = File.open 'app/fixtures/assessment-indicators-and-labels.json'
-    fixture = JSON.load file
-
-    @indicators =
-      fixture.filter { |indicator| indicator['assessment'] == version }
+    @assessments = JSON.load file
   end
 end
 
