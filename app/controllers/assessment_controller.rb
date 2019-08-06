d050871def65619492f554c8068f51f843e590a9
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-06 12:04:15 -0400

    Clean up the template, start checking for country and assessment

--- app/controllers/assessment_controller.rb
+++ app/controllers/assessment_controller.rb
@@ -1,8 +1,9 @@
 class AssessmentController < ApplicationController
   def show
-    #country = params[:country]
+    @country = params[:country]
+    @assessment = params[:assessment]
 
-    file = File.open 'app/fixtures/assessment-indicators-and-labels.json'
+    file = File.open 'app/fixtures/assessments.json'
     @assessments = JSON.load file
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'landing#show'
 
-  get '/assessments(/:country)', to: 'assessment#show'
+  get '/assessments(/:country)(/:assessment)', to: 'assessment#show'
 end
