61b413f009560cef0655bac932331ac9da697970
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-06 15:15:15 -0400

    Connect the scores database table to the view

--- app/controllers/assessment_controller.rb
+++ app/controllers/assessment_controller.rb
@@ -6,7 +6,15 @@ class AssessmentController < ApplicationController
     assessment =
       Assessment.find_by(country: country, assessment_type: assessment_type)
 
-    @scores = assessment.scores if assessment
+    scores = assessment.scores if assessment
+
+    @scores =
+      scores.reduce({}) do |acc, score|
+        key = "#{assessment_type}_ind_#{score['indicator_id']}"
+        acc[key] = score['score']
+        acc
+      end
+
     @data_dictionary = JSON.load File.open './app/fixtures/data_dictionary.json'
     @assessments = JSON.load File.open './app/fixtures/assessments.json'
   end
