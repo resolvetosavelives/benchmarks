abeb4a488b76e18663767f6f2e78699a1d702166
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-06 14:31:18 -0400

    Display scores based on the selected country and assessment_type

--- app/controllers/assessment_controller.rb
+++ app/controllers/assessment_controller.rb
@@ -1,8 +1,12 @@
 class AssessmentController < ApplicationController
   def show
-    @country = params[:country]
-    @assessment = params[:assessment]
+    country = params[:country]
+    assessment_type = params[:assessment]
 
+    assessment =
+      Assessment.find_by(country: country, assessment_type: assessment_type)
+
+    @scores = assessment.scores if assessment
     @data_dictionary = JSON.load File.open './app/fixtures/data_dictionary.json'
     @assessments = JSON.load File.open './app/fixtures/assessments.json'
   end
