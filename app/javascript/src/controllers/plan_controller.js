db838c6e7c6570141ef640319073e10dcdd62ad8
Sid Krishnan <krishnan.sid@gmail.com>
2019-08-07 14:05:37 -0400

    proof of concept for adding activities

--- app/javascript/src/controllers/plan_controller.js
+++ app/javascript/src/controllers/plan_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["activityMap"]
+  static targets = ["activityMap", "newActivity", "submit"]
+
   deleteActivity(e) {
     const { target } = e
     const activityToDelete = target.getAttribute("data-activity")
@@ -9,6 +10,19 @@ export default class extends Controller {
     const newActivityList = this.activityMap[benchmarkId].filter(a => a !== activityToDelete)
     this.activityMapTarget.value = JSON.stringify({...this.activityMap, [benchmarkId]: newActivityList})
     target.closest("tr").classList.add("d-none")
+  }
+
+  showNewActivity() {
+    this.newActivityTarget.classList.remove("d-none")
+  }
+
+  addNewActivity(e) {
+    if (e.keyCode === 13) {
+      this.activityMapTarget.value = JSON.stringify({...this.activityMap, other: [...(this.activityMap.other || []), e.target.value]})
+      e.target.value = ""
+      this.submitTarget.click()
+      e.preventDefault()
+    }
   }
 
   get activityMap(){
