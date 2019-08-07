27827206a30c19ebe5125d90e34966bc34cbfca6
Sid Krishnan <krishnan.sid@gmail.com>
2019-08-07 13:07:23 -0400

    proof of concept for deleting activities using Stimulus

--- app/javascript/src/controllers/plan_controller.js
+++ app/javascript/src/controllers/plan_controller.js
@@ -1,8 +1,17 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["activity"]
+  static targets = ["activityMap"]
   deleteActivity(e) {
-    console.log("Deleting item", event.target.getAttribute("data-activity"))
+    const { target } = e
+    const activityToDelete = target.getAttribute("data-activity")
+    const benchmarkId = target.getAttribute("data-benchmark-id")
+    const newActivityList = this.activityMap[benchmarkId].filter(a => a !== activityToDelete)
+    this.activityMapTarget.value = JSON.stringify({...this.activityMap, [benchmarkId]: newActivityList})
+    target.closest("tr").classList.add("d-none")
+  }
+
+  get activityMap(){
+    return JSON.parse(this.activityMapTarget.value)
   }
 }
