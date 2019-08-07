660d88e8ca5e6101280729c4437b352dde0d9854
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-07 15:56:51 -0400

    Convert score/goal pairs into lists of activities by benchmark area

--- app/controllers/assessment_controller.rb
+++ app/controllers/assessment_controller.rb
@@ -20,15 +20,44 @@ class AssessmentController < ApplicationController
   end
 
   def post
+    crosswalk = JSON.load File.open './app/fixtures/crosswalk.json'
+    benchmarks_and_activities =
+      JSON.load File.open './app/fixtures/benchmarks_and_activities.json'
+
     @country = params.fetch(:country)
     @assessment_type = params.fetch(:assessment)
 
     @params = params
-    @params.delete(:country)
-    @params.delete(:assessment_type)
-    @params.delete(:authenticity_token)
-    @params.delete(:utf8)
-    @params.delete(:controller)
-    @params.delete(:action)
+    params.delete(:country)
+    params.delete(:assessment_type)
+    params.delete(:authenticity_token)
+    params.delete(:utf8)
+    params.delete(:controller)
+    params.delete(:action)
+    params.delete(:assessment)
+
+    # jee1_ind_p11
+    # jee1_ind_p11_goal
+
+    @result =
+      params.keys.reduce([]) do |acc, key|
+        next acc if key.end_with?('_goal')
+        score = params[key].to_i
+        goal = params["#{key}_goal"].to_i
+
+        # raise "key #{key} not found in crosswalk" unless crosswalk[key]
+        next acc unless crosswalk[key]
+        benchmark_ids = crosswalk[key]
+        benchmark_ids.each do |id|
+          activities =
+            (score + 1..goal).reduce([]) do |acc, level|
+              activities = benchmarks_and_activities[id]['capacity'][level.to_s]
+              acc.concat(activities)
+              acc
+            end
+          acc.push({ benchmark_id: id, activities: activities })
+        end
+        acc
+      end
   end
 end
