aa4e76413f9d79ea229120dfcb02b0b5688a5509
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-07 10:24:57 -0400

    Set up a POST operation to receive the results of the assessment

--- app/controllers/assessment_controller.rb
+++ app/controllers/assessment_controller.rb
@@ -1,21 +1,34 @@
 class AssessmentController < ApplicationController
   def show
-    country = params[:country]
-    assessment_type = params[:assessment]
+    @country = params[:country]
+    @assessment_type = params[:assessment]
 
-    assessment =
-      Assessment.find_by(country: country, assessment_type: assessment_type)
+    @assessment =
+      Assessment.find_by(country: @country, assessment_type: @assessment_type)
 
-    scores = assessment.scores if assessment
+    scores = @assessment.scores if @assessment
 
     @scores =
       scores.reduce({}) do |acc, score|
-        key = "#{assessment_type}_ind_#{score['indicator_id']}"
+        key = "#{@assessment_type}_ind_#{score['indicator_id']}"
         acc[key] = score['score']
         acc
       end
 
     @data_dictionary = JSON.load File.open './app/fixtures/data_dictionary.json'
     @assessments = JSON.load File.open './app/fixtures/assessments.json'
+  end
+
+  def post
+    @country = params.fetch(:country)
+    @assessment_type = params.fetch(:assessment)
+
+    @params = params
+    @params.delete(:country)
+    @params.delete(:assessment_type)
+    @params.delete(:authenticity_token)
+    @params.delete(:utf8)
+    @params.delete(:controller)
+    @params.delete(:action)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'landing#show'
   get '/assessments(/:country)(/:assessment)', to: 'assessment#show'
-  resources :plan, only: [:show, :update]
+  post '/assessments(/:country)(/:assessment)', to: 'assessment#post'
+  resources :plan, only: %i[show update]
 end
