bd2886c84896a58df562843de8832d0dfaf9cc6a
Sid Krishnan <krishnan.sid@gmail.com>
2019-08-09 20:07:24 -0400

    serve up stylesheets with webpacker too

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require popper
-//= require bootstrap
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
 //= require_tree .
