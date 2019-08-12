ef39d91b448509a8a5fe16222ac153b7d6543a7e
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-12 13:02:08 -0400

    Clean up the URL for the assessment page
    Involved enabling a route for the country-selector to handle the post
    from the country and assessment selections on the home page.

--- app/controllers/country_select_controller.rb
+++ app/controllers/country_select_controller.rb
@@ -1,9 +1,15 @@
 class CountrySelectController < ApplicationController
   def create
-    @country_name = params.fetch(:country)
+    country_name = params.fetch(:country)
+    assessment_type = params.fetch(:assessment_type)
 
-    assessments = Assessments.find_by(country: @country_name)
-
-    @available_assessments = assessments.map &:assessment_type
+    redirect_to(
+      {
+        controller: 'goals',
+        action: 'show',
+        country: country_name,
+        assessment_type: assessment_type
+      }
+    )
   end
 end
