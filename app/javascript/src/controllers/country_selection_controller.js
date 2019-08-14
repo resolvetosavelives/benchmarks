38e4c7264f1b30b2a8ee0f6ea59663c900074365
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-14 10:41:52 -0400

    Show the modal, populate the form, and be able to submit

--- app/javascript/src/controllers/country_selection_controller.js
+++ app/javascript/src/controllers/country_selection_controller.js
@@ -6,12 +6,17 @@ export default class extends Controller {
   select(e) {
     const countryName = this.selectedCountryTarget.value
     const assessmentTypes = this.selectables[countryName]
+    console.log(`select ${countryName} ${assessmentTypes}`)
     this.assessmentTypesTarget.childNodes.forEach(n =>
       this.assessmentTypesTarget.removeChild(n)
     )
     assessmentTypes.forEach(type =>
       this.assessmentTypesTarget.add(new Option(type))
     )
+
+    $("#assessment-selection-modal #country").val(countryName)
+
+    $("#assessment-selection-modal").modal()
   }
 
   get selectables() {
