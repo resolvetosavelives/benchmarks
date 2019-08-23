3aed7a373dc231883ea27230212fdb6bc37ef731
Savanni D'Gerinel <savanni@cloudcity.io>
2019-08-23 18:37:25 -0400

    Add a Cell manipulation utility and format the costing tool
    Lots of tedious work, but this makes a pretty good-looking costing tool,
    and creates a SpreadsheetCell object that is much easier to work with
    (on the common functions) than the one provided by RubyXL.

--- app/controllers/costsheets_controller.rb
+++ app/controllers/costsheets_controller.rb
@@ -69,77 +69,246 @@ def populate_contents(benchmarks, worksheet, indicators)
 end
 
 def sheet_header(worksheet, capacity_name)
-  worksheet.add_cell(
+  SpreadsheetCell.new(
+    worksheet,
     0,
-    1,
-    'PLANNING AND COSTING TOOL FOR THE DEVELOPMENT OF NATIONAL ACTION PLAN FOR HEALTH SECURITY   2018 - 2022'
+    0,
+    text:
+      'PLANNING AND COSTING TOOL FOR THE DEVELOPMENT OF NATIONAL ACTION PLAN FOR HEALTH SECURITY   2018 - 2022'
   )
-  worksheet.add_cell(1, 1, 'PREVENT')
-  worksheet.add_cell(
+    .with_fill_color('333f50')
+    .with_font_color('000000')
+
+  SpreadsheetCell.new(worksheet, 1, 0, text: 'PREVENT').with_fill_color(
+    '00b0f0'
+  )
+    .with_alignment(nil, 'center')
+
+  SpreadsheetCell.new(
+    worksheet,
     2,
-    1,
-    'General Objective:  To prevent and reduce the likelihood of outbreaks and other public health hazards and events defined by IHR (2005).'
+    0,
+    text:
+      'General Objective:  To prevent and reduce the likelihood of outbreaks and other public health hazards and events defined by IHR (2005).'
   )
-  worksheet.add_cell(3, 1, 'Asset recommendations:')
-  worksheet.add_cell(3, 2, 'User to enter list of recommendations in one cell')
-  worksheet.add_cell(4, 1, 'TECHNICAL AREA')
-  worksheet.add_cell(4, 2, capacity_name)
+    .with_fill_color('00b0f0')
+    .with_font_color('000000')
+    .with_alignment(nil, 'center')
 
-  worksheet.add_cell(4, 14, 'Frequency per year of implementation')
-  worksheet.add_cell(4, 19, 'Annual Cost Estimates')
+  SpreadsheetCell.new(worksheet, 3, 0, text: 'Asset recommendations:')
+    .with_fill_color('dae3f3')
+  SpreadsheetCell.new(
+    worksheet,
+    3,
+    1,
+    text: 'User to enter list of recommendations in one cell'
+  )
+    .with_fill_color('dae3f3')
+  SpreadsheetCell.new(worksheet, 4, 0, text: 'TECHNICAL AREA').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('adb9ca')
+  SpreadsheetCell.new(worksheet, 4, 1, text: capacity_name).with_fill_color(
+    'adb9ca'
+  )
+    .with_font_size(36)
 
-  worksheet.add_cell(5, 1, 'Indicator')
-  worksheet.add_cell(5, 2, 'Objectives')
-  worksheet.add_cell(5, 3, 'Summary of Key Activities (Strategic actions)')
-  worksheet.add_cell(
+  SpreadsheetCell.new(
+    worksheet,
+    4,
+    13,
+    text: 'Frequency per year of implementation'
+  )
+    .with_alignment(nil, 'center')
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 4, 18, text: 'Annual Cost Estimates')
+    .with_alignment(nil, 'center')
+    .with_fill_color('a9d18e')
+
+  SpreadsheetCell.new(worksheet, 5, 0, text: 'Indicator').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(worksheet, 5, 1, text: 'Objectives').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
+    5,
+    2,
+    text: 'Summary of Key Activities (Strategic actions)'
+  )
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
+    5,
+    3,
+    text: 'Detailed activity description (input description for costing)'
+  )
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
     5,
     4,
-    'Detailed activity description (input description for costing)'
+    text:
+      'Detailed cost assumptions (including units, unit costs, quantities, frequency, etc.)'
   )
-  worksheet.add_cell(
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
     5,
     5,
-    'Detailed cost assumptions (including units, unit costs, quantities, frequency, etc.)'
+    text: 'Responsible authority for Implementation (budget holder)'
   )
-  worksheet.add_cell(
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
     5,
     6,
-    'Responsible authority for Implementation (budget holder)'
+    text: 'Implementation scale (National, regional, district, sub-national?)'
   )
-  worksheet.add_cell(
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
     5,
     7,
-    'Implementation scale (National, regional, district, sub-national?)'
+    text:
+      'Comments1 (Potential challenges, comments on implementation arrangements, other)'
   )
-  worksheet.add_cell(
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
     5,
     8,
-    'Comments1 (Potential challenges, comments on implementation arrangements, other)'
+    text:
+      'Comments2 (Potential challenges, comments on implementation arrangements, other)'
   )
-  worksheet.add_cell(
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
     5,
     9,
-    'Comments2 (Potential challenges, comments on implementation arrangements, other)'
+    text: 'Related existing plan/ framework / Programme or on-going activities'
   )
-  worksheet.add_cell(
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(worksheet, 5, 10, text: 'Existing budget(y/n)')
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(
+    worksheet,
     5,
-    10,
-    'Related existing plan/ framework / Programme or on-going activities'
+    11,
+    text: 'Existing budget source (government, donor?)'
   )
-  worksheet.add_cell(5, 11, 'Existing budget(y/n)')
-  worksheet.add_cell(5, 12, 'Existing budget source (government, donor?)')
-  worksheet.add_cell(5, 13, 'Estimated cost (Local currency)')
-  worksheet.add_cell(5, 14, '2018')
-  worksheet.add_cell(5, 15, '2019')
-  worksheet.add_cell(5, 16, '2020')
-  worksheet.add_cell(5, 17, '2021')
-  worksheet.add_cell(5, 18, '2022')
-  worksheet.add_cell(5, 19, 'Cost estimate 2018')
-  worksheet.add_cell(5, 20, 'Cost estimate 2019')
-  worksheet.add_cell(5, 21, 'Cost estimate 2020')
-  worksheet.add_cell(5, 22, 'Cost estimate 2021')
-  worksheet.add_cell(5, 23, 'Cost estimate 2022')
-  worksheet.add_cell(5, 24, 'TotalOver5Years')
-  worksheet.add_cell(5, 25, 'CostCategory1')
-  worksheet.add_cell(5, 26, 'CostCategory2')
+    .with_alignment(nil, 'center')
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(worksheet, 5, 12, text: 'Estimated cost (Local currency)')
+    .with_alignment(nil, 'center')
+    .with_fill_color('ffff00')
+  SpreadsheetCell.new(worksheet, 5, 13, text: '2018').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 14, text: '2019').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 15, text: '2020').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 16, text: '2021').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 17, text: '2022').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 18, text: 'Cost estimate 2018')
+    .with_alignment(nil, 'center')
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 19, text: 'Cost estimate 2019')
+    .with_alignment(nil, 'center')
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 20, text: 'Cost estimate 2020')
+    .with_alignment(nil, 'center')
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 21, text: 'Cost estimate 2021')
+    .with_alignment(nil, 'center')
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 22, text: 'Cost estimate 2022')
+    .with_alignment(nil, 'center')
+    .with_fill_color('a9d18e')
+  SpreadsheetCell.new(worksheet, 5, 23, text: 'TotalOver5Years').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('bdd7ee')
+  SpreadsheetCell.new(worksheet, 5, 24, text: 'CostCategory1').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('d6dce5')
+  SpreadsheetCell.new(worksheet, 5, 25, text: 'CostCategory2').with_alignment(
+    nil,
+    'center'
+  )
+    .with_fill_color('d6dce5')
+
+  worksheet.merge_cells 0, 0, 0, 25
+  worksheet.merge_cells 1, 0, 1, 25
+  worksheet.merge_cells 2, 0, 2, 25
+  worksheet.merge_cells 4, 1, 4, 12
+  worksheet.merge_cells 4, 13, 4, 17
+  worksheet.merge_cells 4, 18, 4, 23
+
+  worksheet.change_row_height(1, 95)
+  worksheet.change_row_height(2, 60)
+  worksheet.change_row_height(4, 47)
+  worksheet.change_row_height(5, 90)
+
+  worksheet.change_column_width(0, 30)
+  worksheet.change_column_width(1, 30)
+  worksheet.change_column_width(2, 25)
+  worksheet.change_column_width(3, 25)
+  worksheet.change_column_width(4, 25)
+  worksheet.change_column_width(5, 25)
+  worksheet.change_column_width(6, 25)
+  worksheet.change_column_width(7, 25)
+  worksheet.change_column_width(8, 25)
+  worksheet.change_column_width(9, 25)
+  worksheet.change_column_width(10, 25)
+  worksheet.change_column_width(11, 25)
+  worksheet.change_column_width(12, 25)
+  worksheet.change_column_width(13, 5.5)
+  worksheet.change_column_width(14, 5.5)
+  worksheet.change_column_width(15, 5.5)
+  worksheet.change_column_width(16, 5.5)
+  worksheet.change_column_width(17, 5.5)
+  worksheet.change_column_width(18, 20)
+  worksheet.change_column_width(19, 20)
+  worksheet.change_column_width(20, 20)
+  worksheet.change_column_width(21, 20)
+  worksheet.change_column_width(22, 20)
+  worksheet.change_column_width(23, 20)
+  worksheet.change_column_width(24, 20)
+  worksheet.change_column_width(25, 20)
 end
---
+++ app/lib/spreadsheet_cell.rb
@@ -0,0 +1,73 @@
+require 'rubyXL/convenience_methods'
+
+class SpreadsheetCell
+  def initialize(worksheet, x, y, params)
+    text = params[:text]
+    formula = params[:formula]
+    raise ArgumentError if text && formula
+
+    if text
+      @cell = worksheet.add_cell(x, y, text)
+      @cell.change_text_wrap true
+    elsif formula
+      @cell = worksheet.add_cell(x, y, '', formula)
+      @cell.change_text_wrap true
+    else
+      @cell = worksheet[x][y]
+      @cell = worksheet.add_cell(x, y) unless @cell
+    end
+  end
+
+  def text
+    @cell.value
+  end
+
+  def formula
+    @cell.formula
+  end
+
+  def with_text(text)
+    @cell.raw_value = text
+    self
+  end
+
+  def with_formula(f)
+    @cell.formula = f
+    self
+  end
+
+  def with_font_color(color)
+    @cell.change_font_color color
+    self
+  end
+
+  def with_font_size(size)
+    @cell.change_font_size size
+    self
+  end
+
+  def with_fill_color(color)
+    @cell.change_fill color
+    self
+  end
+
+  # Valid values:
+  # nil -- defaults, usually to the left and to the bottom of the cell
+  # center
+  def with_alignment(horizontal, vertical)
+    @cell.change_horizontal_alignment horizontal
+    @cell.change_vertical_alignment vertical
+    self
+  end
+
+  def with_border(yes)
+    if yes
+      @cell.change_border(:top, 'thin')
+      @cell.change_border(:bottom, 'thin')
+      @cell.change_border(:left, 'thin')
+      @cell.change_border(:right, 'thin')
+    end
+  end
+end
+
+#  => [:row, :style_index, :datatype, :vm, :ph, :value_container, :worksheet, :row=, :worksheet=, :value_container=, :get_cell_xf, :raw_value=, :get_cell_font, :get_cell_border, :number_format, :is_date?, :is, :index_in_collection, :column, :raw_value, :formula, :inspect, :style_index=, :r, :cm, :datatype=, :cm=, :vm=, :column=, :ph=, :value, :formula=, :r=, :is=, :change_font_name, :change_font_size, :change_font_color, :change_font_italics, :change_fill, :change_font_underline, :font_name, :change_font_strikethrough, :font_size, :change_font_bold, :font_switch, :is_underlined, :text_rotation, :change_contents, :get_border, :get_border_color, :change_horizontal_alignment, :change_vertical_alignment, :change_text_wrap, :change_text_rotation, :change_text_indent, :is_italicized, :is_bolded, :is_struckthrough, :fill_color, :horizontal_alignment, :vertical_alignment, :text_wrap, :text_indent, :set_number_format, :change_border, :font_color, :change_border_color, :workbook, :write_xml, :local_namespaces=, :==, :local_namespaces, :before_write_xml, :to_json, :instance_values, :instance_variable_names, :with_options, :as_json, :`, :present?, :presence, :to_yaml, :acts_like?, :blank?, :in?, :to_param, :presence_in, :to_query, :duplicable?, :deep_dup, :html_safe?, :pretty_print_inspect, :pretty_print, :pretty_print_cycle, :pretty_print_instance_variables, :try, :try!, :load_dependency, :unloadable, :require_or_load, :require_dependency, :instance_variable_defined?, :remove_instance_variable, :instance_of?, :kind_of?, :is_a?, :tap, :instance_variable_get, :instance_variable_set, :protected_methods, :instance_variables, :private_methods, :method, :public_method, :public_send, :singleton_method, :class_eval, :define_singleton_method, :extend, :pretty_inspect, :to_enum, :enum_for, :<=>, :===, :=~, :!~, :gem, :eql?, :respond_to?, :byebug, :remote_byebug, :debugger, :freeze, :object_id, :send, :to_s, :display, :class, :nil?, :hash, :dup, :singleton_class, :clone, :then, :itself, :yield_self, :untaint, :taint, :tainted?, :trust, :untrust, :untrusted?, :singleton_methods, :frozen?, :methods, :public_methods, :equal?, :!, :instance_exec, :!=, :instance_eval, :__id__, :__send__]
