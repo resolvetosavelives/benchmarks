10b5d7b3c21b028a48e82e4d6c633089ce38ca15
Sid Krishnan <krishnan.sid@gmail.com>
2019-08-27 12:33:48 -0400

    add bootstrap-multiselect and get it working

--- app/javascript/src/controllers/filter_controller.js
+++ app/javascript/src/controllers/filter_controller.js
@@ -1,30 +1,41 @@
+import "bootstrap-multiselect"
 import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = ["select"]
 
-  do(e) {
-    const { selectedOptions } = e.currentTarget
-
-    const allOptions = JSON.parse(
-      this.selectTarget.getAttribute("data-options")
-    )
-
-    const optionsToFilter = Array.from(selectedOptions).map(o => o.value)
-    allOptions.forEach(id => {
-      if (optionsToFilter.includes(id)) {
-        this.technicalAreaRows(id).forEach(e => {
-          e.hidden = true
-        })
-      } else {
-        this.technicalAreaRows(id).forEach(e => {
-          e.hidden = false
-        })
-      }
+  connect() {
+    $(this.selectTarget).multiselect({
+      buttonWidth: "100%",
+      maxHeight: 200,
+      inheritClass: true,
+      onChange: filter(this)
     })
   }
 
   technicalAreaRows(id) {
     return Array.from(document.getElementsByClassName(`technical_area_${id}`))
   }
+}
+
+// filter is outside the controller because it appears bootstrap-multiselect
+// changes the `this` context of its `onChange` method
+const filter = controller => () => {
+  const selectedOptions = $(controller.selectTarget).val()
+
+  const allOptions = JSON.parse(
+    controller.selectTarget.getAttribute("data-options")
+  )
+
+  allOptions.forEach(id => {
+    if (selectedOptions.includes(id)) {
+      controller.technicalAreaRows(id).forEach(r => {
+        r.hidden = true
+      })
+    } else {
+      controller.technicalAreaRows(id).forEach(r => {
+        r.hidden = false
+      })
+    }
+  })
 }
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,14 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker")
 
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
+const webpack = require("webpack")
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery",
+    Popper: ["popper.js", "default"]
+  })
+)
 
 module.exports = environment
