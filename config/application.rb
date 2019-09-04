6a37cdb6e1fdafad6dd60b997181754610c1e5cb
Savanni D'Gerinel <savanni@cloudcity.io>
2019-09-04 12:23:42 -0400

    Enable Rack::Attack with a low throttle level

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Src
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.enabled = false
+    config.middleware.use Rack::Attack
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
