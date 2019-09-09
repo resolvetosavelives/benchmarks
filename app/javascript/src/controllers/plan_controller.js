d2be77dcefed660c75c3b9f953bcff2dc7e0a006
Savanni D'Gerinel <savanni@cloudcity.io>
2019-09-09 15:04:27 -0400

    Document the benchmark, filter, and plan JS controllers

--- app/javascript/src/controllers/plan_controller.js
+++ app/javascript/src/controllers/plan_controller.js
@@ -3,6 +3,44 @@ import $ from "jquery"
 
 import renderActivity from "../renderActivity"
 
+/* This controller handles almost all of the in-browser capabilities of the
+ * draft plan form. Users can delete activities, add freeform activities, and
+ * add known activities because of this controller. All changes are stored in
+ * the browser window and will not be saved until the form is submitted.
+ *
+ * This controller hooks some elements based on data attributes instead of
+ * based on targets because of the somewhat sophisticated way it has to deal
+ * with the multiple sections in a draft plan. As best as possible, that
+ * information is documented with the relevant methods.
+ *
+ * Note that the controller does not handle deleting entire benchmarks from the
+ * draft plan. See benchmark_controller.js
+ *
+ * Targets:
+ *   activityMap -- this field contains all of the activities in the plan,
+ *   classified by benchmark ID. The server will populate this field when
+ *   rendering the page, and this controller will modify the field as the user
+ *   edits the draft plan. Note that the benchmark_controller will also edit
+ *   the field.
+ *
+ *   newActivity -- this is a single target that represents all of the fields
+ *   that the user can use to add activities to the plan. Each field must be
+ *   tagged with "data-benchmark-id" so this controller knows which benchmark
+ *   to add the activity to.
+ *
+ *   submit -- the submit button for the form. This controller only needs to be
+ *   able to toggle the enabled/disabled status of the button. To work with
+ *   this controller, the submit button on click should call the `submit`
+ *   function on this controller instead of directly submitting the form. We
+ *   provide this target and the corresponding `submit` method because the
+ *   draft plan layout actually requires that some parts of the "form" live
+ *   outside of the form tag.
+ *
+ *   form -- the functional area of the form. Some parts of the interactive
+ *   part of the form lie outside of this element, but all of the data lies
+ *   within this element.
+ *
+ */
 export default class extends Controller {
   static targets = ["activityMap", "newActivity", "submit", "form"]
 
@@ -55,6 +93,7 @@ export default class extends Controller {
     }
   }
 
+  /* Delete an activity from the plan. */
   deleteActivity(e) {
     const { currentTarget } = e
     const activityToDelete = currentTarget.getAttribute("data-activity")
@@ -74,6 +113,10 @@ export default class extends Controller {
     this.validateActivityMap()
   }
 
+  /* Add a new activity to a benchmark. The add activity form should have a
+   * "data-benchmark-id" tag on it to specify which benchmark to add the
+   * activity to, and must call this function to actually add the text in the
+   * field. */
   addNewActivity(e) {
     const { currentTarget } = e
     const benchmarkId = currentTarget.getAttribute("data-benchmark-id")
@@ -95,28 +138,40 @@ export default class extends Controller {
     }
   }
 
+  /* Get a list of all of the possible activities for a benchmark. */
   autocompletions(benchmarkId) {
     return this.allActivities(benchmarkId).filter(
       a => this.currentActivities(benchmarkId).includes(a) === false
     )
   }
 
+  /* Get the text of all of the current activities for a benchmark in the
+   * activity map. */
   currentActivities(benchmarkId) {
     return this.activityMap[benchmarkId].map(a => a.text)
   }
 
+  /* This provides the list of all possible activities in for a benchmark.
+   *
+   * The "New Activity" field for each benchmark must have a `data-activites`
+   * attribute that that the server must populate with all of the possible
+   * activities for this benchmark.
+   */
   allActivities(benchmarkId) {
     return JSON.parse(
       this.newActivity(benchmarkId).getAttribute("data-activities")
     )
   }
 
+  /* Retrieve a new activity field for the specified benchmark ID. */
   newActivity(benchmarkId) {
     return this.newActivityTargets.find(
       t => t.getAttribute("data-benchmark-id") === benchmarkId
     )
   }
 
+  /* Check the validity of the plan name, disabling the submit button if the
+   * name is invalid. */
   validateName() {
     if (this.formTarget.checkValidity() === false) {
       this.submitTarget.setAttribute("disabled", "disabled")
@@ -126,6 +181,8 @@ export default class extends Controller {
     this.formTarget.classList.add("was-validated")
   }
 
+  /* Check that the draft plan has at least one element in it, and disable the
+   * submit button if it does not. */
   validateActivityMap() {
     if (
       Object.keys(this.activityMap).every(
@@ -138,10 +195,19 @@ export default class extends Controller {
     }
   }
 
+  /* Submit the form.
+   *
+   * We cannot use a normal submit button because the submit button lives
+   * outside of the form on the page. We had to do this for formatting
+   * purposes.
+   */
   submit() {
     this.formTarget.submit()
   }
 
+  /* This causes stimulus to convert the activityMapTarget into a instance
+   * field.
+   */
   get activityMap() {
     return JSON.parse(this.activityMapTarget.value)
   }
