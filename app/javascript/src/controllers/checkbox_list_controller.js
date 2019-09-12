92ff312122059ddca3d76bca05507a5f21c0c206
Savanni D'Gerinel <savanni@cloudcity.io>
2019-09-12 15:56:23 -0400

    Remove some unused lines

--- app/javascript/src/controllers/checkbox_list_controller.js
+++ app/javascript/src/controllers/checkbox_list_controller.js
@@ -1,13 +1,7 @@
 import { Controller } from "stimulus"
 
-const STATE_ALL = "☑️"
-const STATE_SOME = "—"
-const STATE_NONE = "⏹"
-
 export default class extends Controller {
   static targets = ["listItem"]
-
-  connect() {}
 
   selectAll(e) {
     this.listItemTargets.forEach(target => (target.checked = true))
