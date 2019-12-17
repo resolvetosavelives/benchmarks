164d1f9425ee4e5547bb97a1904dd5804b0342b9
Greg Tomei <greg@cloudcity.io>
2019-12-17 12:46:53 -0500

    [Scores Page] Remove the "Get to Green" box #169607204
    - remove JS code for the getToGreen method. fix event argument was poorly named and mis-referenced and probably caused an error.
    - remove styles for .get-to-green class and children. also remove double colon which was prob a typo
    - remove markup for .get-to-green from the goals/show view template and test
    - fix 2 occurences of DOM element IDs being non-unique: country (3 instances), and assessment_type (2 instances). things appear to still work.

--- test/javascript/score_controller.test.js
+++ test/javascript/score_controller.test.js
@@ -76,42 +76,4 @@ describe("ScoreController", () => {
     })
   })
 
-  describe("#go-to-green", () => {
-    beforeEach(() => {
-      document.body.innerHTML = `
-      <div data-controller="score">
-        <button type="button" data-action="click->score#getToGreen" id="gtg"></button>
-        <form data-target="score.form" data-action="submit->score#submit" data-type="jee1">
-        <input id="score1"      type="number" value="1" class="color-score-1" />
-        <input id="score1_goal" type="number" value="2" class="color-score-2" data-goal="true" />
-        <input id="score2"      type="number" value="1" class="color-score-1" />
-        <input id="score2_goal" type="number" value="2" class="color-score-2" data-goal="true" />
-        <input id="score3"      type="number" value="4" class="color-score-4" />
-        <input id="score3_goal" type="number" value="5" class="color-score-5" data-goal="true" />
-        <input id="submit" type="submit" data-target="score.submitButton" />
-      </div>
-      `
-      const application = Application.start()
-      application.register("score", ScoreController)
-    })
-
-    it("sets the value to 4 and sets the correct color", () => {
-      const gtgButton = document.getElementById("gtg")
-      gtgButton.click()
-
-      expect(document.getElementById("score1").value).toEqual("1")
-      expect(document.getElementById("score1_goal").value).toEqual("4")
-      expect(document.getElementById("score1_goal").className).toEqual(
-        "color-score-4"
-      )
-      expect(document.getElementById("score2_goal").value).toEqual("4")
-      expect(document.getElementById("score2_goal").className).toEqual(
-        "color-score-4"
-      )
-      expect(document.getElementById("score3_goal").value).toEqual("5")
-      expect(document.getElementById("score3_goal").className).toEqual(
-        "color-score-5"
-      )
-    })
-  })
 })
