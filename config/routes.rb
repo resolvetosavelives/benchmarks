20de62e2cef521868f5b86ca2210b9adc84491d4
Greg Tomei <greg@cloudcity.io>
2020-02-06 18:17:11 -0500

    Review and tidy up these pages
    - normalized the indentations to use 2 spaces per "tab"
    - fixed indentation in the acknowledgement file, it needed to be indented under the render layout.. part
    - add routes for these two pages
    - add these two pages to the nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get "/privacy_policy", to: "pages#privacy_policy"
-  get "/document/introduction", to: "pages#introduction", as: "introduction"
-  get "/document/1-national-legislation-policy-and-financing", to: "pages#technical_area_1", as: "technical_area_1"
   match "/get_started", to: "plans#get_started", via: [:get, :post]
   get "plan/goals/:country_name/:assessment_type(/:plan_term)(/:areas)", to: "plans#goals", as: "plan_goals"
   resources :plans, only: %i[show index create update destroy]
   resources :worksheets, only: %i[show]
   resources :costsheets, only: %i[show]
   devise_for :users, controllers: {sessions: "users/sessions", registrations: "users/registrations"}
+
+  ##
+  # static pages
+  get "/privacy_policy", to: "pages#privacy_policy"
+  ##
+  # Benchmark Document section
+  get "/document/introduction", to: "pages#introduction", as: "introduction"
+  get "/document/1-national-legislation-policy-and-financing", to: "pages#technical_area_1", as: "technical_area_1"
+  get "/document/acronyms", to: "pages#acronyms", as: "acronyms"
+  get "/document/acknowledgement", to: "pages#acknowledgement", as: "acknowledgement"
 end
