36094ac380ba7cef5db7087e37a1ff5e93be1678
“Stacy <stacy.h.la@gmail.com>
2020-02-12 17:17:30 -0500

    Creation of Technical Areas 4 & 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  match "/get-started", to: "plans#get_started", via: [:get, :post]
+  match "/get_started", to: "plans#get_started", via: [:get, :post]
   get "plan/goals/:country_name/:assessment_type(/:plan_term)(/:areas)", to: "plans#goals", as: "plan_goals"
   resources :plans, only: %i[show index create update destroy]
   resources :worksheets, only: %i[show]
@@ -10,13 +10,14 @@ Rails.application.routes.draw do
   ##
   # static pages
   get "/privacy_policy", to: "pages#privacy_policy"
-  get "/resource-library", to: "pages#resource_library", as: "resource_library"
   ##
   # Benchmark Document section
   get "/document/introduction", to: "pages#introduction", as: "introduction"
   get "/document/1-national-legislation-policy-and-financing", to: "pages#technical_area_1", as: "technical_area_1"
   get "/document/2-ihr-coordination-communication-and-advocacy-and-reporting", to: "pages#technical_area_2", as: "technical_area_2"
   get "/document/3-antimicrobial-resistance", to: "pages#technical_area_3", as: "technical_area_3"
+  get "/document/4-zoonotic-disease", to: "pages#technical_area_4", as: "technical_area_4"
+  get "/document/5-food-safety", to: "pages#technical_area_5", as: "technical_area_5"
   get "/document/acronyms", to: "pages#acronyms", as: "acronyms"
   get "/document/acknowledgement", to: "pages#acknowledgement", as: "acknowledgement"
 end
