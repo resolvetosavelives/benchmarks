688c556313e6044de1c96bfb1f56b71af1683008
Greg Tomei <greg@cloudcity.io>
2020-03-19 11:52:32 -0400

    Add staging env to Sentry environments, will also set SENTRY_CURRENT_ENV=staging for that env to get separate reporting for Staging and Production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = %w[production]
+  config.environments = %w[staging production]
 end
