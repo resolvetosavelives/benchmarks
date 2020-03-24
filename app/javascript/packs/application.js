40042d611b61336663d11598d0a93b996aa0e696
Sid Krishnan <krishnan.sid@gmail.com>
2020-03-24 12:27:24 -0400

    eslint-ignore a few lines in stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,9 +14,9 @@ import "@stimulus/polyfills"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-import $ from "jquery"
+import $ from "jquery" //eslint-disable-line
 import Turbolinks from "turbolinks"
-import Rails from "jquery-ujs"
+import Rails from "jquery-ujs" //eslint-disable-line
 import {} from "jquery-ui/ui/widgets/autocomplete"
 import "chosen-js"
 import "chosen-js/chosen.css"
