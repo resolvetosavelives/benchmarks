fbcc2261c930be9ec4e4c2bea1d183a7cfe874de
Greg Tomei <greg@cloudcity.io>
2020-07-30 22:41:02 -0400

    Draft plan page: Dropdown menu for logged in user does not appear #173633889
    - modify Gemfile to remove gems used in the frontend JS code in favor of NPM packages in packages.json
    - remove jQueryUI from package.json since its no longer used on this project anymore
    - we are no longer using react-rails gem or its rails helper method in the rails template
    - factor out common parts of the central JS to packs/base.js which other packs can import
    - add new JS pack manifests for basic.js and rename application_react.js to application.js
    - simplify packs/application.js by removing code and deps where possible
    - wrap ReactRailsUJS.mountComponents() in a jQuery DomReady callback as a safeguard in case "async: true" is added to scripts again
    - modify application layout to use one JS pack for the home page and application for everything else, just like footers. it works well.
    - add ApplicationHelper to encapsulate logic for JS tags, wisdom about why the JS tags are as such, and footer
    - remove obsolete CSS files and styles from application.scss: autocomplete and multiselect

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,26 +1,14 @@
-/* eslint no-console:0 */
-
-// these two polyfills needed for Internet Explorer 10 to work properly
-// more info on this at: https://babeljs.io/docs/en/babel-polyfill
-import "core-js/stable"
-import "regenerator-runtime/runtime"
-
-import "bootstrap"
-import "stylesheets/application.scss"
+import "./base"
 
 // stimulus polyfill must be loaded first, also needed for IE 10 to work properly
 import "@stimulus/polyfills"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-import $ from "jquery" //eslint-disable-line
-import Turbolinks from "turbolinks"
-import Rails from "jquery-ujs" //eslint-disable-line
-import {} from "jquery-ui/ui/widgets/autocomplete"
+// jquery-ujs is used by some rails form helpers
+import "jquery-ujs" //eslint-disable-line
 import "chosen-js"
 import "chosen-js/chosen.css"
-
-Turbolinks.start()
 
 const application = Application.start()
 const context = require.context("../src/controllers", true, /\.js$/)
@@ -31,3 +19,9 @@ $(".benchmark-document .callout-with-icon svg.bar-chart path").attr(
   "transform",
   "scale(2)"
 )
+
+const componentRequireContext = require.context("src/components", true)
+const ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext) //eslint-disable-line react-hooks/rules-of-hooks
+
+$(ReactRailsUJS.mountComponents)
