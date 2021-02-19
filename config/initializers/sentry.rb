b10d944d2dfba9b4e4b226122f57fd1e9740a541
Liss McCabe <liss@eristiccode.com>
2021-02-19 11:58:17 -0800

    Remove advanced data scrubbing TODO

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,3 @@
 Sentry.init do |config|
-  # TODO: use sentry advanced data scrubbing instead:
-  # https://docs.sentry.io/product/data-management-settings/advanced-datascrubbing/
-  # config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.enabled_environments = %w[staging production]
 end
