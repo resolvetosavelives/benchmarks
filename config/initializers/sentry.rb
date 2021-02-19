1d9f9a8bdf7a2f46893190eda5cc4f59dd9d4370
Liss McCabe <liss@eristiccode.com>
2021-02-19 11:45:34 -0800

    Use sentry-ruby initialization instead of Raven

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,6 @@
-Raven.configure do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = %w[staging production]
+Sentry.init do |config|
+  # TODO: use sentry advanced data scrubbing instead:
+  # https://docs.sentry.io/product/data-management-settings/advanced-datascrubbing/
+  # config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.enabled_environments = %w[staging production]
 end
