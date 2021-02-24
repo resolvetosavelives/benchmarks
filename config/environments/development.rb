7e7165f5f76abe91b95eb495d924531b09423ed3
Andre Arko <andre@arko.net>
2021-02-24 01:02:55 -0800

    allow puma-dev to host app at benchmarks.test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,4 +86,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow the app in puma-dev running at https://benchmarks.test
+  config.hosts << "benchmarks.test"
 end
