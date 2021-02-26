1d764740595470277a54252e0d99a156185d65c8
Andre Arko <andre@arko.net>
2021-02-26 12:40:32 -0800

    submit the country form during the test

--- test/system/apps_test.rb
+++ test/system/apps_test.rb
@@ -13,6 +13,8 @@ class AppsTest < ApplicationSystemTestCase
     click_on("Get Started") until current_path == "/get-started"
     assert page.has_content?("LET'S GET STARTED")
     select_from_chosen("Nigeria", from: "get_started_form_country_id")
+    click_on("Next")
+
     choose "Joint External Evaluation (JEE)"
     choose "1 year plan"
 
