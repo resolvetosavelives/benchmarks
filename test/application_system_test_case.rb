6dbc969de94f143b4bcc5497fe8784bce122cc94
Andre Arko <andre@arko.net>
2021-03-11 14:59:56 -0800

    wait longer and disable inspector to help CI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -12,12 +12,13 @@ Capybara.register_driver(:cuprite) do |app|
   Capybara::Cuprite::Driver.new(
     app,
     window_size: [1200, 800],
-    # See additional options for Dockerized environment in the respective section of this article
     browser_options: {},
     # Increase Chrome startup wait time (required for stable CI builds)
     process_timeout: 30,
-    # Enable debugging capabilities
-    inspector: true,
+    # Tell Ferrum to wait longer for browsers to respond, helps on slow machines like CI
+    timeout: 10,
+    # Enable debugging only outside CI by default
+    inspector: ENV['CI'].nil? || ENV['INSPECTOR'],
     # Allow running Chrome in a headful mode by setting HEADLESS env
     # var to a falsey value
     headless: !ENV['HEADLESS'].in?(%w[n 0 no false])
