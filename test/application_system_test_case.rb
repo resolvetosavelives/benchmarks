06b71823583c927642dca4fc697012e786383452
Andre Arko <andre@arko.net>
2021-03-11 14:50:03 -0800

    allow at least 30 seconds for Rails to boot

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -15,7 +15,7 @@ Capybara.register_driver(:cuprite) do |app|
     # See additional options for Dockerized environment in the respective section of this article
     browser_options: {},
     # Increase Chrome startup wait time (required for stable CI builds)
-    process_timeout: 10,
+    process_timeout: 30,
     # Enable debugging capabilities
     inspector: true,
     # Allow running Chrome in a headful mode by setting HEADLESS env
