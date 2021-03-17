0d5dfe027067d211b18196599239e5c16247fa49
André Arko <andre@arko.net>
2021-03-16 17:17:23 -0700

    Fix bugs, add tests (#283)
    * fix error on technical area 17 page
    * standardize class order
    * use bootstrap grid to implement responsive design
      for more explanation of how this works, see:
      https://getbootstrap.com/docs/4.0/layout/grid/
    * update caniuse-lite database
    * reorder routes
      the `root` should always go last, because Rails eager matches rules first to last. devise should always go first, so that your auth routes will win even if you accidentally declare something else with the same name. the rest don't necessarily have a required order, but dynamic first and static last seems reasonable.
    * make prettier ignore routes.rb because YIKES
    * add a test for every single static page
    * it should not be possible to merge broken haml that renders as a 500
    error! this hopefully makes that true :)

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,73 +1,31 @@
-require "test_helper"
+require 'test_helper'
 
 describe PagesController do
-  describe "#home" do
-    it "is connected as the root URL" do
-      assert_routing("/", { controller: "pages", action: "home" })
+  describe '#home' do
+    it 'is connected as the root URL' do
+      assert_routing('/', { controller: 'pages', action: 'home' })
     end
 
-    it "responds with success" do
+    it 'responds with success' do
       get root_url
       assert_response :success
     end
   end
 
-  describe "#privacy_policy" do
-    it "is connected as /privacy_policy" do
-      assert_routing(
-        "/privacy_policy",
-        { controller: "pages", action: "privacy_policy" },
-      )
-    end
+  routes =
+    Rails
+      .application
+      .routes
+      .routes
+      .map { |r| r.path.spec.to_s.sub('(.:format)', '') }
+  static_routes = routes.grep(%r{^/(privacy|reference-library|document/)})
 
-    it "respond with success" do
-      get privacy_policy_url
-      assert_response :success
-    end
-  end
-
-  describe "benchmarks introduction page" do
-    it "is connected at the expected URL" do
-      assert_routing(
-        "/document/introduction",
-        { controller: "pages", action: "introduction" },
-      )
-    end
-
-    it "responds with success" do
-      get introduction_url
-      assert_response :success
-      assert_template "pages/introduction"
-    end
-  end
-
-  describe "benchmarks technical area page 1" do
-    it "is connected at the expected URL" do
-      assert_routing(
-        "/document/1-national-legislation-policy-and-financing",
-        { controller: "pages", action: "technical_area_1" },
-      )
-    end
-
-    it "responds with success" do
-      get technical_area_1_url
-      assert_response :success
-      assert_template "pages/technical_area_1"
-    end
-  end
-
-  describe "reference library" do
-    it "is connected at the expected URL" do
-      assert_routing(
-        "/reference-library",
-        { controller: "pages", action: "reference_library" },
-      )
-    end
-
-    it "responds with success" do
-      get reference_library_url
-      assert_response :success
-      assert_template "pages/reference_library"
+  static_routes.each do |path|
+    describe "path #{path}" do
+      it 'renders successfully' do
+        get path
+        assert_response :success
+      end
     end
   end
 end
