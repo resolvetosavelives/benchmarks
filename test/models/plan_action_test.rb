82450e202b3f4dae81901018562daf3f103fcf86
Andre Arko <andre@arko.net>
2021-03-24 21:28:46 -0700

    fix minitest warnings

--- test/models/plan_action_test.rb
+++ test/models/plan_action_test.rb
@@ -6,17 +6,17 @@ describe PlanAction do
   describe '.new_for_benchmark_action' do
     it 'returns a hash of the expected structure' do
       bia = BenchmarkIndicatorAction.first
-      bia.wont_be_nil
-      bia.benchmark_indicator.wont_be_nil
-      bia.benchmark_indicator.benchmark_technical_area.wont_be_nil
+      _(bia).wont_be_nil
+      _(bia.benchmark_indicator).wont_be_nil
+      _(bia.benchmark_indicator.benchmark_technical_area).wont_be_nil
       result = PlanAction.new_for_benchmark_action bia
 
       _(result).must_be_instance_of PlanAction
-      result.benchmark_indicator_action.wont_be_nil
+      _(result.benchmark_indicator_action).wont_be_nil
       _(result.benchmark_indicator_action).must_equal bia
-      result.benchmark_indicator_id.wont_be_nil
+      _(result.benchmark_indicator_id).wont_be_nil
       _(result.benchmark_indicator_id).must_equal bia.benchmark_indicator_id
-      result.benchmark_technical_area_id.wont_be_nil
+      _(result.benchmark_technical_area_id).wont_be_nil
       _(result.benchmark_technical_area_id).must_equal bia
                                                          .benchmark_technical_area_id
     end
