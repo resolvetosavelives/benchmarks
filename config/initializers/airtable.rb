3268d9f79ccf0e7fafbf69940527d29067d6883d
Andre Arko <andre@arko.net>
2021-04-08 15:18:08 -0700

    build some working API records and a hash that can feed into the db

--- config/initializers/airtable.rb
+++ config/initializers/airtable.rb
@@ -1,14 +1,31 @@
-module Airtable
-  def self.client
-    @client ||=
-      begin
-        key = ENV.fetch("AIRTABLE_API_KEY")
-        client = Airtable::Client.new(key)
-      end
-  end
+Airrecord.api_key = ENV.fetch("AIRTABLE_API_KEY")
 
-  def self.reference_documents
-    @reference_documents ||=
-      client.table("tblJxOZGxd8HWE1qs", "Reference Documents")
+module Airtable
+  class ReferenceLibraryDocuments < Airrecord::Table
+    self.base_key = "appD3gX9b3YbXUlj1"
+    self.table_name = "Form"
+
+    def self.fetch_approved(since: nil)
+      filter = "IS_AFTER(LAST_MODIFIED_TIME(), #{since.utc.iso8601})" if since
+      self.all(view: "Approved", filter: filter)
+    end
+
+    def to_attrs
+      attachment = self["Attachments"].first
+
+      {
+        title: self["Document Title"],
+        description: self["Description"],
+        author: self["Author"],
+        date: self["Publication Date"],
+        relevant_pages: self["Relevant Pages"],
+        technical_area: self["Technical Area"],
+        document_type: self["Document Type"],
+        download_url: attachment & ["url"],
+        thumbnail_url: attachment & ["thumbnails"] & ["large"] & ["url"],
+        url: self["URL"],
+        last_modified: self["Last Modified"]
+      }
+    end
   end
 end
