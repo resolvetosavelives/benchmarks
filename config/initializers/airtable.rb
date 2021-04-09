0e17b056013e06ee6d86ccbd4fe3297c5d1e5e89
Andre Arko <andre@arko.net>
2021-04-08 22:49:25 -0700

    make airtable api key optional in development

--- config/initializers/airtable.rb
+++ config/initializers/airtable.rb
@@ -1,1 +1,5 @@
-Airrecord.api_key = ENV.fetch("AIRTABLE_API_KEY")
+if Rails.env.production?
+  Airrecord.api_key = ENV.fetch("AIRTABLE_API_KEY")
+else
+  Airrecord.api_key = ENV["AIRTABLE_API_KEY"]
+end
