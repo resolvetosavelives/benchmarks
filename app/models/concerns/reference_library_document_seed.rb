980f3528d203a3535089dec9372289beb78c1bb5
Andre Arko <andre@arko.net>
2021-04-08 22:57:42 -0700

    add rake task to update reference docs from airtable

--- app/models/concerns/reference_library_document_seed.rb
+++ app/models/concerns/reference_library_document_seed.rb
@@ -10,5 +10,18 @@ module ReferenceLibraryDocumentSeed
     def unseed!
       ReferenceLibraryDocument.destroy_all
     end
+
+    def update_from_airtable!
+      last_edit =
+        ReferenceLibraryDocument.order(:last_modified).last&.last_modified
+      new_docs =
+        Airtable::ReferenceLibraryDocument.fetch_approved(since: last_edit)
+      return if new_docs.empty?
+
+      now = Time.now
+      doc_attrs =
+        new_docs.map { |d| d.to_attrs.merge(created_at: now, updated_at: now) }
+      ReferenceLibraryDocument.insert_all(doc_attrs)
+    end
   end
 end
