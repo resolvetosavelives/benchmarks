87b4c996c12a01bc6b8e93868104d17d43b0116d
Andre Arko <andre@arko.net>
2021-04-12 16:00:38 -0700

    fast Assessment seeds via JSON from xlsx files

--- app/models/concerns/assessment_seed.rb
+++ app/models/concerns/assessment_seed.rb
@@ -8,12 +8,30 @@ module AssessmentSeed
       return unless Assessment.count.zero?
 
       warn "Seeding data for Assessments..."
-      seed_jee "data/JEE scores Mar 2021.xlsx"
-      seed_spar "data/spar/SPAR Data 2018_2019July9.xlsx",
-        "data/spar/SPAR Data 2019_2021Mar29.xlsx"
+      data =
+        JSON.parse Rails.root.join("data/asssessments_and_scores.json").read
+      Assessment.bulk_import data["assessments"].map { |d| Assessment.new(d) }
+      AssessmentScore.bulk_import data["scores"].map { |d|
+                                    AssessmentScore.new(d)
+                                  }
     end
 
-    def seed_jee(path, update: false)
+    def write_seed!
+      File.write "data/asssessments_and_scores.json",
+                 JSON.dump(
+                   assessments: Assessment.all.as_json,
+                   scores: AssessmentScore.all.as_json
+                 )
+    end
+
+    def update_seed!
+      update_jee "data/JEE scores Mar 2021.xlsx"
+      update_spar "data/spar/SPAR Data 2018_2019July9.xlsx",
+                  "data/spar/SPAR Data 2019_2021Mar29.xlsx"
+      write_seed!
+    end
+
+    def update_jee(path)
       file = Rails.root.join(path)
       sheet = RubyXL::Parser.parse(file).worksheets.first
       legend = sheet[0].cells.map(&:value)
@@ -39,14 +57,14 @@ module AssessmentSeed
         data
           .except("iso", "version")
           .each do |id, value|
-          ai = AssessmentIndicator.find_by_code!(pub.named_id, id)
-          score =
-            assessment
-            .scores
-            .create_with(value: value)
-            .find_or_create_by!(assessment_indicator: ai)
-          score.update(value: value) if update
-        end
+            ai = AssessmentIndicator.find_by_code!(pub.named_id, id)
+            score =
+              assessment
+                .scores
+                .create_with(value: value)
+                .find_or_create_by!(assessment_indicator: ai)
+            score.update(value: value)
+          end
       end
     end
 
@@ -61,31 +79,31 @@ module AssessmentSeed
             [l.downcase.tr(".", ""), v / 20]
           end.to_h
 
-          name = row.cells[2].value.strip
-          corrections = {
-            "Democratic Republic of the Congo" =>
+        name = row.cells[2].value.strip
+        corrections = {
+          "Democratic Republic of the Congo" =>
             "Congo, Democratic Republic of the",
-              "United Republic of Tanzania" => "Tanzania, United Republic of",
-              "Micronesia" => "Micronesia (Federated States of)",
-              "Democratic People's Republic of Korea" =>
+          "United Republic of Tanzania" => "Tanzania, United Republic of",
+          "Micronesia" => "Micronesia (Federated States of)",
+          "Democratic People's Republic of Korea" =>
             "Korea (Democratic People's Republic of)",
-              "Republic of Korea" => "Korea, Republic of",
-              "Czech Republic" => "Czechia",
-              "Republic of Moldova" => "Moldova, Republic of",
-              "Guinea Bissau" => "Guinea-Bissau",
-              "Saint Vicent and the Grenadines" =>
+          "Republic of Korea" => "Korea, Republic of",
+          "Czech Republic" => "Czechia",
+          "Republic of Moldova" => "Moldova, Republic of",
+          "Guinea Bissau" => "Guinea-Bissau",
+          "Saint Vicent and the Grenadines" =>
             "Saint Vincent and the Grenadines",
-              "Venezuela (Bolivarian Republique of)" =>
+          "Venezuela (Bolivarian Republique of)" =>
             "Venezuela (Bolivarian Republic of)",
-              "Iran (Islamic Republic of )" => "Iran (Islamic Republic of)"
-          }
-          attrs["name"] = corrections[name] || name
+          "Iran (Islamic Republic of )" => "Iran (Islamic Republic of)"
+        }
+        attrs["name"] = corrections[name] || name
 
-          self.new(attrs)
+        self.new(attrs)
       end
     end
 
-    def seed_spar(*paths, update: false)
+    def update_spar(*paths)
       spar_data = {}
 
       paths.each do |path|
@@ -99,7 +117,7 @@ module AssessmentSeed
             [r.name, r] if r
           end.to_h
 
-          spar_data.merge!(data)
+        spar_data.merge!(data)
       end
 
       spar = AssessmentPublication.find_by_named_id!("spar_2018")
@@ -112,10 +130,10 @@ module AssessmentSeed
           ai = AssessmentIndicator.find_by_code!("spar_2018", id)
           score =
             assessment
-            .scores
-            .create_with(value: value)
-            .find_or_create_by!(assessment_indicator: ai)
-          score.update(value: value) if update
+              .scores
+              .create_with(value: value)
+              .find_or_create_by!(assessment_indicator: ai)
+          score.update(value: value)
         end
       end
     end
