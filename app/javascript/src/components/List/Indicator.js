fbc47022b1328087b20ff41992c5a097ef6a75c9
Liss McCabe <liss@eristiccode.com>
2021-04-13 23:21:00 -0700

    wip start adding badge pills for current and target scores on benchmark indicators

--- app/javascript/src/components/List/Indicator.js
+++ app/javascript/src/components/List/Indicator.js
@@ -1,9 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 import IndicatorActionList from "./IndicatorActionList"
+import rightArrow from "./right-arrow.svg"
 
 const Indicator = (props) => {
   const indicator = props.indicator
+  const currentScore = "3"
+  const targetScore = "4"
 
   return (
     <div
@@ -11,6 +14,15 @@ const Indicator = (props) => {
       data-benchmark-indicator-display-abbrev={indicator.display_abbreviation}
     >
       <div className="row bg-light-gray px-2 header">
+        <div className="col-1 d-flex flex-row align-items-center justify-content-end py-2 py-md-0">
+          <span className="badge badge-pill badge-primary align-middle rounded-circle mx-auto">
+            {currentScore}
+          </span>
+          <img src={rightArrow} />
+          <div className="badge badge-pill badge-secondary align-middle">
+            {targetScore}
+          </div>
+        </div>
         <div className="col-11">
           <b>Benchmark {indicator.display_abbreviation}:</b>
           &nbsp;
