2918355dd48a44800eb6707e6ba2fd37e57f8405
Liss McCabe <liss@eristiccode.com>
2021-04-15 21:50:00 -0700

    Wistfully created factories, longing for simpler tests

---
+++ test/factories/assessment_publication_factory.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :assessment_publication do
+    title { "SpellCardFairnessEvaluation" }
+    named_id { "spfe_2021" }
+    abbrev { "SPFE" }
+    revision { "2021" }
+  end
+end
