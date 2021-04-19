9c0a75c308ac8da1535ad72d84ef88bcd7072abb
Andre Arko <andre@arko.net>
2021-04-19 13:55:37 -0700

    update migration to use new seed update methods

--- db/migrate/20210401231030_update_scores.rb
+++ db/migrate/20210401231030_update_scores.rb
@@ -2,9 +2,8 @@ class UpdateScores < ActiveRecord::Migration[6.1]
   include AssessmentSeed::ClassMethods
 
   def change
-    seed_spar "data/spar/SPAR Data 2018_2019July9.xlsx",
-              "data/spar/SPAR Data 2019_2021Mar29.xlsx",
-              update: true
-    seed_jee "data/JEE scores Mar 2021.xlsx", update: true
+    update_spar "data/spar/SPAR Data 2018_2019July9.xlsx",
+                "data/spar/SPAR Data 2019_2021Mar29.xlsx"
+    update_jee "data/JEE scores Mar 2021.xlsx"
   end
 end
