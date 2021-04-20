ac534de9dc3f9fc0a9f14af3e90d69bee69ed371
Andre Arko <andre@arko.net>
2021-04-20 01:10:28 -0700

    more design tweaks for remove banner, but remove doesn't work?

--- app/javascript/src/components/List/Action.js
+++ app/javascript/src/components/List/Action.js
@@ -5,8 +5,8 @@ import { getIndicatorMap } from "../../config/selectors"
 import { useDispatch, useSelector } from "react-redux"
 import ActionBadge from "./ActionBadge"
 import ActionBadgePill from "./ActionBadgePill"
-import { CSSTransition } from 'react-transition-group';
-import { Button } from 'react-bootstrap'
+import { CSSTransition } from "react-transition-group"
+import { Button } from "react-bootstrap"
 
 const Action = (props) => {
   const action = props.action
@@ -14,6 +14,10 @@ const Action = (props) => {
   const indicatorMap = useSelector((state) => getIndicatorMap(state))
   const indicator = indicatorMap[action.benchmark_indicator_id]
   const [showDelete, setShowDelete] = useState(false)
+
+  function removeAction() {
+    dispatch(deleteAnAction(action.id, indicator.id))
+  }
 
   function deleteBanner() {
     return (
@@ -26,13 +30,24 @@ const Action = (props) => {
         <div className="delete-banner p-2 d-flex align-items-center">
           <div className="col-8">
             <h4 className="my-2">Remove this action?</h4>
-            <p>You can always add this action back from the &ldquo;Add an activity&rdquo; form below.</p>
+            <p>
+              You can always add this action back from the &ldquo;Add an
+              activity&rdquo; form below.
+            </p>
           </div>
           <div className="col-4 d-flex justify-content-end">
-            <Button className="px-3 py-2" onClick={() => dispatch(deleteAnAction(action.id, indicator.id))}>
+            <Button
+              variant="remove"
+              className="mx-2 px-3 py-2"
+              onClick={removeAction}
+            >
               Remove
             </Button>
-            <Button className="px-3 py-2" variant="link" onClick={() => setShowDelete(false)}>
+            <Button
+              variant="cancel"
+              className="mx-2 px-3 py-2"
+              onClick={() => setShowDelete(false)}
+            >
               Cancel
             </Button>
           </div>
@@ -51,16 +66,14 @@ const Action = (props) => {
             &nbsp;
             <span className="action-text">{action.text}</span>
           </div>
-          <div className="col-1 d-flex flex-row align-items-center justify-content-end py-2 py-md-0 ">
+          <div className="col-2 d-flex flex-row align-items-center justify-content-end py-2 py-md-0 ">
             {action.disease_id ? (
               <ActionBadgePill action={action} />
             ) : (
               <ActionBadge action={action} />
             )}
-          </div>
-          <div className="col-1 d-flex d-md-block py-2 py-md-0">
             <button
-              className="delete close"
+              className="delete close ml-3"
               type="button"
               onClick={() => setShowDelete(true)}
             >
