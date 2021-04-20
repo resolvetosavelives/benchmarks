44c80b3258eee35211cb176e2e0c7e0ef9653570
Andre Arko <andre@arko.net>
2021-04-20 01:10:01 -0700

    remove banner, with animation

--- app/javascript/src/components/List/Action.js
+++ app/javascript/src/components/List/Action.js
@@ -1,21 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { deleteAnAction } from "../../config/actions"
 import { getIndicatorMap } from "../../config/selectors"
 import { useDispatch, useSelector } from "react-redux"
 import ActionBadge from "./ActionBadge"
 import ActionBadgePill from "./ActionBadgePill"
+import { CSSTransition } from 'react-transition-group';
+import { Button } from 'react-bootstrap'
 
 const Action = (props) => {
   const action = props.action
   const dispatch = useDispatch()
   const indicatorMap = useSelector((state) => getIndicatorMap(state))
   const indicator = indicatorMap[action.benchmark_indicator_id]
+  const [showDelete, setShowDelete] = useState(false)
+
+  function deleteBanner() {
+    return (
+      <CSSTransition
+        in={showDelete}
+        timeout={1000}
+        classNames="confirm-delete"
+        unmountOnExit
+      >
+        <div className="delete-banner p-2 d-flex align-items-center">
+          <div className="col-8">
+            <h4 className="my-2">Remove this action?</h4>
+            <p>You can always add this action back from the &ldquo;Add an activity&rdquo; form below.</p>
+          </div>
+          <div className="col-4 d-flex justify-content-end">
+            <Button className="px-3 py-2" onClick={() => dispatch(deleteAnAction(action.id, indicator.id))}>
+              Remove
+            </Button>
+            <Button className="px-3 py-2" variant="link" onClick={() => setShowDelete(false)}>
+              Cancel
+            </Button>
+          </div>
+        </div>
+      </CSSTransition>
+    )
+  }
 
   return (
     <div className="row action">
+      {deleteBanner()}
       <div className="col">
-        <div className="row p-2">
+        <div className="row p-4">
           <div className="col-10">
             <strong>{indicator.display_abbreviation}</strong>
             &nbsp;
@@ -32,7 +62,7 @@ const Action = (props) => {
             <button
               className="delete close"
               type="button"
-              onClick={() => dispatch(deleteAnAction(action.id, indicator.id))}
+              onClick={() => setShowDelete(true)}
             >
               <img src="/delete-button.svg" alt="Delete this action" />
             </button>
