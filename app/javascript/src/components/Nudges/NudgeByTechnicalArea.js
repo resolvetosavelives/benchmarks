aee155e65015c57d34d094a4637cc75f67c7a889
Andre Arko <andre@arko.net>
2021-04-28 20:14:22 -0700

    add responsive nudges for phone

--- app/javascript/src/components/Nudges/NudgeByTechnicalArea.js
+++ app/javascript/src/components/Nudges/NudgeByTechnicalArea.js
@@ -12,13 +12,13 @@ const NudgeByTechnicalArea = () => {
     whichNudgeComponent = <NudgeByTechnicalAreaOneYear />
   }
   return (
-    <>
-      <div className="col-4 col-lg-3 d-flex flex-column justify-content-center nudge-left">
+    <div className="row m-0">
+      <div className="col-12 col-md-4 col-lg-3 d-flex flex-column justify-content-center nudge-left px-4 py-4">
         <div className="nudge-tip-text">Tips for</div>
         <h4 className="my-0">Your draft plan</h4>
       </div>
-      <div className="col-8 col-lg-9">{whichNudgeComponent}</div>
-    </>
+      <div className="col-12 col-md-8 col-lg-9">{whichNudgeComponent}</div>
+    </div>
   )
 }
 
