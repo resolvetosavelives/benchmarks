8215f953bb7abe038090858e7826b0ea2b8aef5c
Liss McCabe <liss@eristiccode.com>
2021-04-30 11:48:49 -0700

    Increase Cuprite timeout, maybe

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,10 +1,10 @@
-require 'test_helper'
+require "test_helper"
 
-require 'capybara/cuprite'
-require 'capybara_helper'
-require 'minitest/rails/capybara'
+require "capybara/cuprite"
+require "capybara_helper"
+require "minitest/rails/capybara"
 
-Capybara.default_max_wait_time = 2
+Capybara.default_max_wait_time = 5
 Capybara.default_normalize_ws = true
 Capybara.default_driver = Capybara.javascript_driver = :cuprite
 
@@ -16,12 +16,12 @@ Capybara.register_driver(:cuprite) do |app|
     # Increase Chrome startup wait time (required for stable CI builds)
     process_timeout: 30,
     # Tell Ferrum to wait longer for browsers to respond, helps on slow machines like CI
-    timeout: 10,
+    timeout: 15,
     # Enable debugging only outside CI by default
-    inspector: ENV['CI'].nil? || ENV['INSPECTOR'],
+    inspector: ENV["CI"].nil? || ENV["INSPECTOR"],
     # Allow running Chrome in a headful mode by setting HEADLESS env
     # var to a falsey value
-    headless: !ENV['HEADLESS'].in?(%w[n 0 no false])
+    headless: !ENV["HEADLESS"].in?(%w[n 0 no false])
   )
 end
 
@@ -34,28 +34,28 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   def save_state_from_server(page)
     state_from_server = extract_state_from_server(page)
-    write_state_to_file('state_from_server', state_from_server)
+    write_state_to_file("state_from_server", state_from_server)
   end
 
   def save_state_influenza(page)
     state_from_server = extract_state_from_server(page)
-    write_state_to_file('state_from_server_with_influenza', state_from_server)
+    write_state_to_file("state_from_server_with_influenza", state_from_server)
   end
 
   def save_state_influenza_and_cholera(page)
     state_from_server = extract_state_from_server(page)
     write_state_to_file(
-      'state_from_server_with_influenza_and_cholera',
+      "state_from_server_with_influenza_and_cholera",
       state_from_server
     )
   end
 
   def extract_state_from_server(page)
-    script_element = page.find('#state_from_server', visible: false)
+    script_element = page.find("#state_from_server", visible: false)
     state_from_server_js_content = script_element.native.all_text
-    first_bracket_position = state_from_server_js_content.index('{')
+    first_bracket_position = state_from_server_js_content.index("{")
     last_bracket_position_reversed =
-      state_from_server_js_content.reverse.index('}')
+      state_from_server_js_content.reverse.index("}")
     last_bracket_position =
       state_from_server_js_content.size - last_bracket_position_reversed
     last_bracket_position -= 1 # to chop off the semicolon
@@ -68,8 +68,8 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   def write_state_to_file(file_name, file_data)
     state_from_server_json_file =
-      Rails.root.join('test', 'fixtures', 'files', "#{file_name}.json")
-    File.open(state_from_server_json_file, 'w') do |f|
+      Rails.root.join("test", "fixtures", "files", "#{file_name}.json")
+    File.open(state_from_server_json_file, "w") do |f|
       f.write(JSON.pretty_generate(file_data))
     end
   end
