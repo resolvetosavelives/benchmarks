baa3b6c839f7446bfa99d3aab33a9b22cd9fee0d
Gregory Tomei <greg@graybike.com>
2021-08-11 14:15:27 -0400

    The pipeline mostly works (it is triggered properly and builds all tasks) but the test task cannot connect to the DB. Tried using DATABASE_URL in many different variations, also tried adjusting database.yml to use url.

--- config/terraform/benchmarks-app-service-azure.tf
+++ config/terraform/benchmarks-app-service-azure.tf
@@ -33,8 +33,18 @@ resource "azurerm_postgresql_server" "WhoIhrBenchmarksDbServer" {
   name                = "who-ihr-benchmarks-db-server"
   location            = azurerm_resource_group.WhoIhrBenchmarks.location
   resource_group_name = azurerm_resource_group.WhoIhrBenchmarks.name
+//  attempting to use the line "public_network_access_enabled = false" results in:
+//  azurerm_postgresql_server.WhoIhrBenchmarksDbServer: Creating...
+//╷
+//│ Error: creating PostgreSQL Server "who-ihr-benchmarks-db-server" (Resource Group "WhoIhrBenchmarks"): postgresql.ServersClient#Create: Failure sending request: StatusCode=0 -- Original Error: Code="FeatureSwitchNotEnabled" Message="Requested feature is not enabled"
+//│
+//│   with azurerm_postgresql_server.WhoIhrBenchmarksDbServer,
+//│   on benchmarks-app-service-azure.tf line 55, in resource "azurerm_postgresql_server" "WhoIhrBenchmarksDbServer":
+//│   55: resource "azurerm_postgresql_server" "WhoIhrBenchmarksDbServer" {
+//│
+//  public_network_access_enabled = false
 
-  sku_name = "B_Gen5_2"
+  sku_name = "B_Gen5_2" // or B_Gen5_1
 
   storage_mb                   = 5120
   backup_retention_days        = 7
@@ -47,10 +57,31 @@ resource "azurerm_postgresql_server" "WhoIhrBenchmarksDbServer" {
   administrator_login_password = "Lo4YdiJcggrN9TfPagCW2AsMtFbQ8R6N"
   version                      = "11"
   ssl_enforcement_enabled      = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
 }
-
-resource "azurerm_postgresql_database" "benchmarks_production" {
-  name                = "benchmarks_production"
+resource "azurerm_postgresql_firewall_rule" "WhoIhrBenchmarksDbServerFirewallRuleAllowAzure" {
+  name                = "allow-access-from-azure-services"
+  resource_group_name = azurerm_resource_group.WhoIhrBenchmarks.name
+  server_name         = azurerm_postgresql_server.WhoIhrBenchmarksDbServer.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+resource "azurerm_postgresql_firewall_rule" "WhoIhrBenchmarksDbServerFirewallRuleAllowGregHome" {
+  name                = "allow-access-from-greg-home"
+  resource_group_name = azurerm_resource_group.WhoIhrBenchmarks.name
+  server_name         = azurerm_postgresql_server.WhoIhrBenchmarksDbServer.name
+  start_ip_address    = "71.182.150.16"
+  end_ip_address      = "71.182.150.16"
+}
+resource "azurerm_postgresql_firewall_rule" "WhoIhrBenchmarksDbServerFirewallRuleAllowAll" {
+  name                = "allow-access-from-all"
+  resource_group_name = azurerm_resource_group.WhoIhrBenchmarks.name
+  server_name         = azurerm_postgresql_server.WhoIhrBenchmarksDbServer.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
+resource "azurerm_postgresql_database" "benchmarks_test" {
+  name                = "benchmarks_test"
   resource_group_name = azurerm_resource_group.WhoIhrBenchmarks.name
   server_name         = azurerm_postgresql_server.WhoIhrBenchmarksDbServer.name
   charset             = "UTF8"
@@ -73,6 +104,12 @@ resource "azurerm_container_registry" "acr" {
 #
 # SECTION: the cloud applications that depend upon the infrastructure
 #
+variable "GITHUB_AUTH_PERSONAL" {
+  type = string
+}
+variable "DATABASE_URL" {
+  type = string
+}
 resource "azuredevops_project" "project" {
   name = "WhoIhrBenchmarks001"
   description = "WHO IHR Benchmarks Project"
@@ -94,6 +131,11 @@ resource "azuredevops_git_repository" "repository" {
 resource "azuredevops_serviceendpoint_github" "serviceendpoint_gh_1" {
   project_id = azuredevops_project.project.id
   service_endpoint_name = "Benchmarks GitHub repo"
+  # this is a real Personal access tokens to Github that expires in 90 days
+  auth_personal {
+    personal_access_token = var.GITHUB_AUTH_PERSONAL
+  }
+//  auth_personal = "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"
 }
 
 // build_definition this is what devops.azure.com calls a pipeline
@@ -111,5 +153,18 @@ resource "azuredevops_build_definition" "build_definition" {
     branch_name = "pipeline-create-and-setup--178993699--build"
     yml_path    = "azure-pipelines.yml"
     service_connection_id = azuredevops_serviceendpoint_github.serviceendpoint_gh_1.id
+  }
+
+//  variable {
+//    name = "RAILS_ENV"
+//    value = var.RAILS_ENV
+//    allow_override = true
+//  }
+  variable {
+    name = "DATABASE_URL"
+//    is_secret = true
+//    secret_value = var.DATABASE_URL
+    value = var.DATABASE_URL
+    allow_override = false
   }
 }
