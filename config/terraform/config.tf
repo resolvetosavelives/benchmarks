7d08bd962b305ee1b4be7b40a0688ff80e59e02a
Gregory Tomei <greg@graybike.com>
2021-08-26 12:45:09 -0400

    WIP: break up terraform from one file for everything to three files: one for centralized config, one for the main infrastructure, and one for devops-related stuff. next will make use of terraform workspaces to have environments.
    WIP on Private Link for ACR (supposedly should be working now) and DB (not yet up)..
    
    Got ACR working with docker authentication but publicly accessible so will have to continue to keep secrets out of docker images. add Postgres DB server instance on a Private Endpoint, is working via VPN only which is good. VPN via MacOS is working and stable so thats huge, works via IP but does not seem to be able to resolve internal domains/FQDNs. Need to use a General Purpose sku for the Postgres DB server to get network/IP/private-endpoints. currently using Private DNS Zone for DB but does not seem to make a difference with VPN/remote conns.
    
    Change branch for the build pipeline to use my current topic branch, azure--virtual-network-and-subnets--179242298
    
    Now with VNet and Subnets in place, and Private Link for the DB, bring all the other cloud resources back up. App Service and Build Pipeline are there but still not fully working as before, a known issue which we'll tackle in a separate story.

---
+++ config/terraform/config.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.74"
+    }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.7"
+    }
+  }
+  // terraform state stored securely in azure storage and is encrypted in transit and at rest.
+  //  backend "azurerm" {
+  //    resource_group_name = "who_ihr_benchmarks"
+  //    storage_account_name = "tfstate5b92c0"
+  //    container_name = "tfstate"
+  //    key = "terraform.tfstate"
+  //  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "89789ead-0e38-4e72-8fd9-3cdcbe80b4ef"
+  tenant_id       = "7018baf0-4beb-46d2-a7d1-7679026af9e0"
+}
+// to expose to other tf code: id, tenant_id, subscription_id, display_name..
+data "azurerm_subscription" "current" {}
+
+//
+// See the README.md file for the conventions for naming things and code style.
+//
+locals {
+  env                 = terraform.workspace == "production" ? "production" : "sandbox"
+  app_name            = "who-ihr-benchmarks"
+  resource_group_name = "${local.app_name}-${local.env}"
+}
