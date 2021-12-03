ca96694a0e490e46a06b2b7d062bdd4704de6820
Gregory Tomei <greg@cloudcity.io>
2021-12-03 14:48:07 -0500

    minor tweaks

--- config/terraform/README.md
+++ config/terraform/README.md
@@ -1,7 +1,7 @@
 # How to Use This: Provisioning and Managing This Project's Azure Deployments with Terraform
 
 ```
- azuredevops_project.project "IHRBENCHMARK"
+terraform import azuredevops_project.project "IHRBENCHMARK"
 terraform import azuredevops_serviceendpoint_github.serviceendpoint_for_who_github 679f7d50-14c7-4b17-aea0-cc0e0452141b/0d3d26b6-fa89-4314-b91f-dc18e069a176
 ```
 
--- config/terraform/config.tf
+++ config/terraform/config.tf
@@ -14,19 +14,23 @@ terraform {
     }
   }
   // terraform state stored securely in azure storage and is encrypted in transit and at rest.
-  backend "azurerm" {
-    // Variables not allowed in this block
-    resource_group_name  = "WHOIHRBENCHMARKS-TERRAFORM-EUW-RG01"
-    storage_account_name = "tfstate5b92c0"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+#  backend "azurerm" {
+#    // Variables not allowed in this block
+#    resource_group_name  = "WHOIHRBENCHMARKS-TERRAFORM-EUW-RG01"
+#    storage_account_name = "tfstate5b92c0"
+#    container_name       = "tfstate"
+#    key                  = "terraform.tfstate"
+#  }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "974ebced-5bea-4fa8-af6f-7064aa3eccff"
-  tenant_id       = "f610c0b7-bd24-4b39-810b-3dc280afb590"
+#  # WHO sub and tenant
+#  subscription_id = "974ebced-5bea-4fa8-af6f-7064aa3eccff"
+#  tenant_id       = "f610c0b7-bd24-4b39-810b-3dc280afb590"
+  # my personal sub and tenant
+  subscription_id = "89789ead-0e38-4e72-8fd9-3cdcbe80b4ef"
+  tenant_id       = "7018baf0-4beb-46d2-a7d1-7679026af9e0"
 }
 // to expose to other tf code: id, tenant_id, subscription_id, display_name..
 data "azurerm_subscription" "current" {}
