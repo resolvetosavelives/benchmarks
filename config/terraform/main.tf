0cf579f8a6aae97efd0876ce989db56525e96eed
Martin <martin.emde@cloudcity.io>
2022-01-21 15:36:32 -0800

    upgrade azurerm terraform provider

--- config/terraform/main.tf
+++ config/terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.74"
+      version = "~> 2.93"
     }
     random = {
       source  = "hashicorp/random"
