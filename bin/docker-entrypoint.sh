a11ceabc69ea0be716378b364d9b811c1ac11001
Martin <martin.emde@cloudcity.io>
2022-01-26 10:10:31 -0800

    Use relative paths in docker-entrypoint

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f ../tmp/pids/server.pid
 
-bin/rails db:migrate 2>/dev/null || bin/rails db:create db:setup
+./rails db:migrate 2>/dev/null || ./rails db:create db:setup
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
