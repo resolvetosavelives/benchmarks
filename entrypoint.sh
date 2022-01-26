359e30daf5784d393ed3f6c0cf94d5494b4469f5
Martin <martin.emde@cloudcity.io>
2022-01-25 17:34:18 -0800

    Try using entrypoint to run migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+bin/rails db:migrate 2>/dev/null || bin/rails db:create db:setup
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
