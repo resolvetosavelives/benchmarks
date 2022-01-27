4b601398bf7c3ec85a58d55b38779def8f4553e0
Martin <martin.emde@cloudcity.io>
2022-01-27 11:33:02 -0800

    Go back to the 'gentle' entrypoint that doesn't force reset the database

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f tmp/pids/server.pid
 
-#(echo "Migrating Database..." && bin/rails db:migrate 2>/dev/null) || (echo "Creating database..." && bin/rails db:create db:setup)
-# First we'll try non-destructive
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:environment:set db:schema:load db:seed
+# This command resets the db when migrations apply partially on a new database.
+#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:environment:set db:schema:load db:seed
+(echo "Migrating Database..." && bin/rails db:migrate 2>/dev/null) || (echo "Creating database..." && bin/rails db:create db:environment:set db:schema:load db:seed)
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
