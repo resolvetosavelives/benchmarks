fd8ddceefe11235e17628236cace3081bea4c8c3
Martin <martin.emde@cloudcity.io>
2022-01-27 09:23:54 -0800

    Try more forceful reset of production database

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,7 @@ rm -f tmp/pids/server.pid
 
 #(echo "Migrating Database..." && bin/rails db:migrate 2>/dev/null) || (echo "Creating database..." && bin/rails db:create db:setup)
 # First we'll try non-destructive
-#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:environment:set db:schema:load db:seed
-bin/rails db:environment:set db:seed
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:environment:set db:schema:load db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
