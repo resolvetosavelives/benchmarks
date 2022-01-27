d88f1921f29d9306a6c6c8c75755e929757e115f
Martin <martin.emde@cloudcity.io>
2022-01-26 16:40:36 -0800

    Reset the production database

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f tmp/pids/server.pid
 
-(echo "Migrating Database..." && bin/rails db:migrate 2>/dev/null) || (echo "Creating database..." && bin/rails db:create db:setup)
+#(echo "Migrating Database..." && bin/rails db:migrate 2>/dev/null) || (echo "Creating database..." && bin/rails db:create db:setup)
+# First we'll try non-destructive
+#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:environment:set db:schema:load db:seed
+bin/rails db:environment:set db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
