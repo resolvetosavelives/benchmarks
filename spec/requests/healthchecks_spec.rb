e11de09b45a9426161c6e5cd2443bdef5125a2e0
Martin <martin.emde@cloudcity.io>
2022-02-18 11:36:51 -0800

    omg, I forgot to check in the updated spec

--- spec/requests/healthchecks_spec.rb
+++ spec/requests/healthchecks_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Healthchecks", type: :request do
   describe "GET /healthchecks" do
     it "returns 200" do
       get healthcheck_path
-      expect(response).to have_http_status(204)
+      expect(response).to have_http_status(:success)
     end
   end
 end
