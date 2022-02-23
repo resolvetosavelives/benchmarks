ef98d1fd585e15cf083b5ebc80943d06ebe81864
Martin <martin.emde@cloudcity.io>
2022-02-22 21:09:20 -0800

    Import the svg circle and update links.
    I haven't verified the behavior yet because my asset pipeline isn't working

--- app/javascript/packs/basic.js
+++ app/javascript/packs/basic.js
@@ -2,19 +2,16 @@ import "./base"
 import "particles.js"
 
 const assessText =
-  "<h3>Assess capacity</h3><p>Assessments such as JEE/SPAR to  identify and evaluate a country’s national preparedness capacities, identify risks and prioritize actions for capacity building to prevent, detect, and rapidly respond to public health and threats.</p>"
+  "<h3>Assess capacity</h3>" +
+  "<p>Countries must assess their ability to prevent, detect and respond to epidemics using different approaches that provide unique insights into the country’s risk, vulnerability and capacity profile for health emergency threats.</p>"
 
-const planText =
-  "<h3>Prioritize actions and convene for planning</h3><p>Identify and prioritize key benchmark activities based on JEE or SPAR assessment to guide and facilitate a convening and consensus-building process with cross-sectoral teams for a 1-year operational plan or 5-year NAPHS plan.</p>"
-
-const costText =
-  "<h3>Estimate costs</h3><p>Identify all available domestic and international resources (financial and technical) that are needed and estimate the costs to support decision making and prioritization.</p>"
+const developText =
+  "<h3>Prioritize actions for health security preparedness strengthening</h3>" +
+  "<p>Generate priority actions for a 5-year strategic plan or a 1-year operational plan using the WHO Benchmarks digital tool, convene key actors to build political will across sectors and identify the resources necessary for systems change to support planning and resource allocation for national plans.</p>"
 
 const implementText =
-  "<h3>Implement capacity building actions</h3><p>Use relevant guidance, tools, frameworks and other materials in the reference library that can inform the implementation of benchmark actions.</p>"
-
-const monitorText =
-  "<h3>Monitor and evaluate</h3><p>Continuously monitor and evaluate performance of national action plan.</p>"
+  "<h3>Implement prioritized actions</h3>" +
+  "<p>Use relevant best practices, guidelines, tools, and training packages to support the implementation of prioritized actions. As you implement your plan, continuously monitor and evaluate performance.</p>"
 
 $(() => {
   document
@@ -23,24 +20,16 @@ $(() => {
       document.getElementById("step-up-text").innerHTML = assessText
     })
 
-  document.getElementById("plan-element").addEventListener("mouseenter", () => {
-    document.getElementById("step-up-text").innerHTML = planText
-  })
-
-  document.getElementById("cost-element").addEventListener("mouseenter", () => {
-    document.getElementById("step-up-text").innerHTML = costText
-  })
+  document
+    .getElementById("develop-element")
+    .addEventListener("mouseenter", () => {
+      document.getElementById("step-up-text").innerHTML = developText
+    })
 
   document
     .getElementById("implement-element")
     .addEventListener("mouseenter", () => {
       document.getElementById("step-up-text").innerHTML = implementText
-    })
-
-  document
-    .getElementById("monitor-element")
-    .addEventListener("mouseenter", () => {
-      document.getElementById("step-up-text").innerHTML = monitorText
     })
 
   particlesJS.load("particles-js", "/particles.json") //eslint-disable-line
