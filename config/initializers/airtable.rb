5010cd98648f91c4e11de8ef81d88df8deddded9
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2023-03-14 14:30:06 -0500

    Only check for credentials if env vars are not set
    Fixes errors in Heroku build

--- config/initializers/airtable.rb
+++ config/initializers/airtable.rb
@@ -1,6 +1,8 @@
 Airrecord.api_key =
   if Rails.env.production? && !Rails.application.config.asset_compilation
-    ENV.fetch("AIRTABLE_PAT", Rails.application.credentials.airtable_api_key!)
+    ENV.fetch("AIRTABLE_PAT") do
+      Rails.application.credentials.airtable_api_key!
+    end
   else
     ENV.fetch("AIRTABLE_PAT", Rails.application.credentials.airtable_api_key)
   end
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,18 @@
 Sentry.init do |config|
   config.enabled_environments = %w[staging production]
   if Rails.env.production? && !Rails.application.config.asset_compilation
-    config.dsn = Rails.application.credentials.sentry_dsn!
-    config.environment = Rails.application.credentials.sentry_current_env!
+    config.dsn =
+      ENV.fetch("SENTRY_DSN") { Rails.application.credentials.sentry_dsn! }
+    config.environment =
+      ENV.fetch("SENTRY_CURRENT_ENV") do
+        Rails.application.credentials.sentry_current_env!
+      end
   else
-    config.dsn = Rails.application.credentials.sentry_dsn
-    config.environment = Rails.application.credentials.sentry_current_env
+    config.dsn =
+      ENV.fetch("SENTRY_DSN") { Rails.application.credentials.sentry_dsn }
+    config.environment =
+      ENV.fetch("SENTRY_CURRENT_ENV") do
+        Rails.application.credentials.sentry_current_env
+      end
   end
 end
